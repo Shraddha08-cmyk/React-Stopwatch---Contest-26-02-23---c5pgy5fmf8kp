df35e1fa8a3b549063c52884cf2a4b93149ee416
Shraddha Tiwari <114010000+Shraddha08-cmyk@users.noreply.github.com>
2023-02-26 15:26:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import '../styles/App.css';
 const App = () => {
   const startTime = useRef(0);
